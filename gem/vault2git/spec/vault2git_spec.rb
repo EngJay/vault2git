f091934b06daddb7d13b9cb0331ce1b07143ffa1
Jason Scott <jpscott07@gmail.com>
2022-06-11 19:49:25 -0400

    Generate default gem in subfolder

---
+++ gem/vault2git/spec/vault2git_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Vault2git do
+  it "has a version number" do
+    expect(Vault2git::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
