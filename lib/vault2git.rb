b7a3d80265fa5a560e425a1bb6a8434755927bcf
Jason Scott <jpscott07@gmail.com>
2022-06-12 07:26:40 -0400

    Move original source into gem lib

--- lib/vault2git.rb
+++ lib/vault2git.rb
@@ -1,8 +1,14 @@
 # frozen_string_literal: true
 
 require_relative "vault2git/version"
+require_relative "vault2git/options"
+require_relative "vault2git/converter"
 
 module Vault2git
   class Error < StandardError; end
   # Your code goes here...
 end
+
+# options = Options.new
+# converter = Converter.new(options.options)
+# converter.convert
