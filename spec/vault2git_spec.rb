cbf97688d067c54e8f6f7c2c0d13ee354ab14ca7
Jason Scott <jpscott07@gmail.com>
2022-06-12 07:27:06 -0400

    Add first spec for options class

--- spec/vault2git_spec.rb
+++ spec/vault2git_spec.rb
@@ -1,11 +1,26 @@
 # frozen_string_literal: true
 
+require "vault2git"
+require "vault2git/options"
+
 RSpec.describe Vault2git do
   it "has a version number" do
     expect(Vault2git::VERSION).not_to be nil
   end
+end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+RSpec.describe Vault2git::Options do
+  # options = Vault2git::Options.new
+
+  it "should exit cleanly when instantiated without arguments" do
+    expect { Vault2git::Options.new }.to raise_error(SystemExit) do |error|
+      expect(error.status).to eq(0) # Clean exit.
+    end
+
+    # expect(options.username).to eq("")
+    # expect(options.password).to eq("")
+    # expect(options.vault_client).to eq('C:\\Program Files\\SourceGear\\Vault Client\\vault.exe')
+    # expect(options.git).to eq("git")
+    # expect(options.logfile).to eq("vault2git.log")
   end
 end
