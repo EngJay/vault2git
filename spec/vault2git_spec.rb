3a344ec3aca5943a06f421c14c95202dd4831d52
Jason Scott <jpscott07@gmail.com>
2022-06-13 22:12:26 -0400

    Add specs for Options

--- spec/vault2git_spec.rb
+++ spec/vault2git_spec.rb
@@ -18,11 +18,82 @@ RSpec.describe Vault2git::Options do
     expect { Vault2git::Options.new }.to raise_error(SystemExit) do |error|
       expect(error.status).to eq(0) # Clean exit.
     end
+  end
 
-    # expect(options.username).to eq("")
-    # expect(options.password).to eq("")
-    # expect(options.vault_client).to eq('C:\\Program Files\\SourceGear\\Vault Client\\vault.exe')
-    # expect(options.git).to eq("git")
-    # expect(options.logfile).to eq("vault2git.log")
+  it "should exit cleanly when instantiated with one argument" do
+    # Remove the "spec" arg set by rspec.
+    ARGV.clear
+    ARGV.append("spec_source")
+
+    expect { Vault2git::Options.new }.to raise_error(SystemExit) do |error|
+      expect(error.status).to eq(0) # Clean exit.
+    end
+  end
+
+  it "should construct options with defaults with two arguments and no options" do
+    # Remove the "spec" arg set by rspec.
+    ARGV.clear
+    ARGV.append("spec_source")
+    ARGV.append("spec_dest")
+
+    options = Vault2git::Options.new.options
+
+    expect(options.username).to eq("")
+    expect(options.password).to eq("")
+    expect(options.vault_client).to eq('C:\\Program Files\\SourceGear\\Vault Client\\vault.exe')
+    expect(options.git).to eq("git")
+    expect(options.logfile).to eq("vault2git.log")
+  end
+
+  it "should construct options with defaults with two arguments and username and password" do
+    # Remove the "spec" arg set by rspec, then set just the source arg.
+    ARGV.clear
+    ARGV.append("--username")
+    ARGV.append("mr_the_plague")
+    ARGV.append("--password")
+    ARGV.append("acid_burn")
+    ARGV.append("spec_source")
+    ARGV.append("spec_dest")
+
+    options = Vault2git::Options.new.options
+
+    expect(options.username).to eq("mr_the_plague")
+    expect(options.password).to eq("acid_burn")
+    expect(options.vault_client).to eq("C:\\Program Files\\SourceGear\\Vault Client\\vault.exe")
+    expect(options.git).to eq("git")
+    expect(options.logfile).to eq("vault2git.log")
+  end
+
+  it "should construct options with two arguments and additonal options" do
+    # Remove the "spec" arg set by rspec, then set just the source arg.
+    ARGV.clear
+    ARGV.append("--username")
+    ARGV.append("mr_the_plague")
+    ARGV.append("--password")
+    ARGV.append("acid_burn")
+    ARGV.append("--vault-client-path")
+    ARGV.append("C:\\hack\\the\\planet.exe")
+    ARGV.append("--git-path")
+    ARGV.append("non\\standard\\git\\path")
+    ARGV.append("--logfile")
+    ARGV.append("non-default-filename.txt")
+    ARGV.append("spec_source")
+    ARGV.append("spec_dest")
+
+    options = Vault2git::Options.new.options
+
+    expect(options.username).to eq("mr_the_plague")
+    expect(options.password).to eq("acid_burn")
+    expect(options.vault_client).to eq("C:\\hack\\the\\planet.exe")
+    expect(options.git).to eq("non\\standard\\git\\path")
+    expect(options.logfile).to eq("non-default-filename.txt")
   end
 end
+
+# RSpec.describe Vault2git::Converter do
+# it "should exit cleanly when instantiated without arguments" do
+#   expect { Vault2git::Options.new }.to raise_error(SystemExit) do |error|
+#     expect(error.status).to eq(0) # Clean exit.
+#   end
+# end
+# end
