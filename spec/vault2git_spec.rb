b2073bd1e56f6b8acb3e1203b983bef7f55d7670
Jason Scott <jpscott07@gmail.com>
2022-06-14 20:35:00 -0400

    Add specs for Converter#quote_param

--- spec/vault2git_spec.rb
+++ spec/vault2git_spec.rb
@@ -2,6 +2,7 @@
 
 require "vault2git"
 require "vault2git/options"
+require "vault2git/converter"
 
 RSpec.describe Vault2git do
   it "has a version number" do
@@ -90,10 +91,78 @@ RSpec.describe Vault2git::Options do
   end
 end
 
-# RSpec.describe Vault2git::Converter do
-# it "should exit cleanly when instantiated without arguments" do
-#   expect { Vault2git::Options.new }.to raise_error(SystemExit) do |error|
-#     expect(error.status).to eq(0) # Clean exit.
-#   end
-# end
-# end
+RSpec.describe Vault2git::Converter do
+  before do
+    suppress_log_output
+  end
+  
+  describe "#quote_param" do
+    
+    it "should return the values of the options" do  
+      # Remove the "spec" arg set by rspec, then set just the source arg.
+      ARGV.clear
+      ARGV.append("--username")
+      ARGV.append("mr_the_plague")
+      ARGV.append("--password")
+      ARGV.append("acid_burn")
+      ARGV.append("--vault-client-path")
+      ARGV.append("C:\\hack\\the\\planet.exe")
+      ARGV.append("--git-path")
+      ARGV.append("non\\standard\\git\\path")
+      ARGV.append("--logfile")
+      ARGV.append("non-default-filename.txt")
+      ARGV.append("spec_source")
+      ARGV.append("spec_dest")
+
+      options = Vault2git::Options.new.options
+      converter = Vault2git::Converter.new(options)
+
+      expected_username = converter.quote_param(:username)
+      expect(expected_username).to eq("mr_the_plague")
+
+      expected_password = converter.quote_param(:password)
+      expect(expected_password).to eq("acid_burn")
+
+      expected_vault_client = converter.quote_param(:vault_client)
+      expect(expected_vault_client).to eq("C:\\hack\\the\\planet.exe")
+
+      expected_git = converter.quote_param(:git)
+      expect(expected_git).to eq("non\\standard\\git\\path")
+
+      expected_logfile = converter.quote_param(:logfile)
+      expect(expected_logfile).to eq("non-default-filename.txt")
+    end
+
+    it "should return the values of the arguments" do  
+      # Remove the "spec" arg set by rspec, then set just the source arg.
+      ARGV.clear
+      ARGV.append("--username")
+      ARGV.append("mr_the_plague")
+      ARGV.append("--password")
+      ARGV.append("acid_burn")
+      ARGV.append("--vault-client-path")
+      ARGV.append("C:\\hack\\the\\planet.exe")
+      ARGV.append("--git-path")
+      ARGV.append("non\\standard\\git\\path")
+      ARGV.append("--logfile")
+      ARGV.append("non-default-filename.txt")
+      ARGV.append("spec_source")
+      ARGV.append("spec_dest")
+
+      options = Vault2git::Options.new.options
+      converter = Vault2git::Converter.new(options)
+
+      expected_source = converter.quote_param(:source)
+      expect(expected_source).to eq("spec_source")
+
+      expected_dest = converter.quote_param(:dest)
+      expect(expected_dest).to eq("spec_dest")
+    end
+  end
+
+  # def quote_value(value)
+  #   return "" unless value
+
+  #   value.include?(" ") ? "\"#{value}\"" : value
+  # end
+end
